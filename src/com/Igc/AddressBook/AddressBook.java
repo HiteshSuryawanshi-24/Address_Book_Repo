b4d000c9a180061cf611d0e870f22bab728dd556
HiteshSuryawanshi-24 <hitesh291998@gmail.com>
2022-03-09 16:00:30 +0530

    MODIFIED: Edit Contact for Address Book

--- src/com/Igc/AddressBook/AddressBook.java
+++ src/com/Igc/AddressBook/AddressBook.java
@@ -3,7 +3,7 @@ package com.Igc.AddressBook;
 import java.util.Scanner;
 
 public class AddressBook {
-    Scanner sc = new Scanner(System.in);
+    public Scanner sc = new Scanner(System.in);
     Contact contact = new Contact();
 
 
@@ -28,11 +28,39 @@ public class AddressBook {
     public void displayContact(){
         System.out.println(contact.toString());
     }
+    public void editContact(){
+        System.out.println("Edit Contatct");
+        System.out.println("Enter the Name");
+        String name= sc.next();
 
+        if (name.equalsIgnoreCase(contact.getFirstName())){
+            System.out.println("Enter the First Name:");
+            contact.setFirstName(sc.next());
+            System.out.println("Enter the Last Name:");
+            contact.setLastName(sc.next());
+            System.out.println("Enter the Address:");
+            contact.setAddress(sc.next());
+            System.out.println("Enter the City Name:");
+            contact.setCity(sc.next());
+            System.out.println("Enter the State Name:");
+            contact.setState(sc.next());
+            System.out.println("Enter the Zip No:");
+            contact.setZip(sc.next());
+            System.out.println("Enter the Phone Number:");
+            contact.setPhoneno(sc.next());
+            System.out.println("Enter the Email Id:");
+            contact.setEmailId(sc.next());
+        }
+        else {
+            System.out.println("Contact Not Found");
+        }
+    }
 
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         addressBook.addContact();
+        addressBook.displayContact();
+        addressBook.editContact();
         addressBook.displayContact();
     }
 }
